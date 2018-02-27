3a9757cf6d7eba69bf7ec8953fc200470176fb22
Nikaoto <nikaoto@gmail.com>
2018-02-28 01:59:16 +0400

    Initial commit

---
+++ scrapers/bing.js
@@ -0,0 +1,21 @@
+const bing = require("nodejs-bing")
+const Promise = require("bluebird")
+
+const debugMode = false
+
+function log(str) {
+  return debugMode ? console.log(str) : null
+}
+
+const scrape = (query) => {
+  return new Promise(resolve => {
+    log(`scraping: ${query}`)
+
+    bing.image(query).then(res => {
+      res.forEach(el => log(el.mediaurl))
+      return resolve(res)
+    })
+  })
+}
+
+module.exports = scrape
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const scrapeBing = require("./scrapers/bing")
+
+const PORT = process.env.PORT || 1000
+const server = express()
+
+server.use(bodyParser.json())
+server.use(cors())
+
+server.get("/", (req, res) => {
+  res.send("Hi :^)")
+  res.end()
+})
+
+server.get("/bing", ({ query }, res) => {
+  console.log("GET /bing :", query)
+
+  const max = parseInt(query.max) || 1
+
+  scrapeBing(query.query.toString())
+  .then(scrapeResult => res.json(scrapeResult.slice(0, max)))
+  .catch(err => { 
+    console.log("err")
+    res.json("Error") 
+  })
+})
+
+server.listen(PORT, () => {
+  console.log("Listening on port", PORT)
+})
