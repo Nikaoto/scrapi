ae54895028f14a7af8d99106559eb827a5cf962f
Nikaoto <nikaoto@gmail.com>
2018-03-10 18:42:57 +0400

    rewrite whole server

--- server.js
+++ server.js
@@ -1,113 +1,79 @@
-const DEV_MODE = (process.env.NODE_ENV !== "production")
-if (DEV_MODE) {
-  require("dotenv").load()
-}
+const PORT = process.env.PORT || 2000
 
-const express = require("express")
 const fetch = require("node-fetch")
-const path = require("path")
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const express = require("express")
 
+// Scrapper(s)
 const scrapeBing = require("./scrapers/bing")
 
-const aws = require("aws-sdk")
-const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME
-const AWS_REGION = process.env.AWS_REGION
-aws.config.region = AWS_REGION
 
-const PORT = process.env.PORT || 2000
-//const DOMAIN = DEV_MODE ? "https://localhost:"+PORT : "https://scr-api.herokuapp.com:"+PORT
-const server = express()
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
 
-//const fs = require("fs")
-// Create downloads folder
-// if (!fs.existsSync("./downloads")) {
-//   fs.mkdirSync("./downloads")
-// }
-//server.use(express.static(path.join(__dirname, "downloads")))
-
-server.use(bodyParser.json())
-server.use(cors())
-
-server.get("/", (req, res) => {
-  res.send("Hi :^)")
-  res.end()
+app.get("/", (req, res) => {
+  res.end("Hi :^)")
 })
 
-server.get("/bing", ({ query }, res) => {
+
+app.get("/images", ({ query }, response) => {
   console.log("GET /bing :", query)
 
   const max = parseInt(query.max) || 1
 
   scrapeBing(query.query.toString())
-    .then(scrapeResult => res.json(scrapeResult.slice(0, max)))
-    .catch(err => { 
-      console.log("err")
-      res.json("Error") 
+  .then(scrapeResult => {
+    const results = scrapeResult.slice(0, max).map(res =>
+      ({
+        url: res.mediaurl,
+        title: res.title,
+        source: res.link,
+        info: res.size
+      })
+    )
+
+    response.json(results)
+
+  }).catch(err => { 
+    console.log("err")
+    response.json("Error") 
+  })
+})
+
+app.get("/images-2", (req, response) => {
+  fetch(linkFromQuery(req.query.query), fetchOptions)
+  .then(res => res.json())
+  .then(res => {
+    const img = res.value[0]
+    console.log(img)
+    console.log("datePublished", img.datePublished)
+    console.log("encodingFormat:", img.encodingFormat)
+    console.log("thumbnailUrl:", img.thumbnailUrl)
+    console.log("thumbnailSize:", img.thumbnail)
+    console.log("source:", img.hostPageUrl)
+    console.log("url:", img.contentUrl)
+
+    response.json({
+      "url": img.contentUrl,
+      "title": img.name,
+      "thumbnailUrl": img.thumbnailUrl,
+      "source": img.hostPageUrl,
     })
-})
-
-server.get("/list", (req, res) => {
-  console.log("GET /list bucket:", S3_BUCKET_NAME)
-  const s3 = new aws.S3()
-  s3.listObjects({ Bucket: S3_BUCKET_NAME }, (err, data) => {
-    if (err) {
-      console.log("ERROR:", err)
-      return
-    }
-
-    console.log(data)
-    res.json(data)
   })
+  .catch(err => console.log(err))
 })
 
-const getS3Url = (fileKey) => `https://s3.${AWS_REGION}.amazonaws.com/${S3_BUCKET_NAME}/${encodeURIComponent(fileKey)}`
-
-server.get("/download", ({ query }, res) => {
-  console.log("GET /download :", query)
-  //const fileName = query.url.replace(/\/|:/g, "")
-  const imageKey = encodeURIComponent(query.url)
-  console.log("imageKey:", imageKey)
-
-
-  const s3 = new aws.S3()
-  s3.listObjects({ Bucket: S3_BUCKET_NAME }, (err, data) => {
-    if (err) {
-      console.log("ERROR:", err)
-      return
-    }
-
-    const fileExists = data.Contents.map(obj => obj.Key).includes(imageKey)
-
-    if (fileExists) {
-      console.log("File already exists, returning url")
-      res.json({ url: getS3Url(imageKey) })
-      return
-    }
-
-    console.log("File doesn't exist, starting upload")
-
-    fetch(query.url)
-      .then(img => {
-        // TODO promisify this
-        const params = { Bucket: S3_BUCKET_NAME, Key: imageKey, Body: img.body, ACL:"public-read" }
-        s3.upload(params, (err, data) => { 
-          if (err) {
-            console.log("ERROR while uploading:", err)
-            res.end()
-          } else {
-            console.log("Upload successful! data:", data)
-            res.json({ url: data.Location })
-          }
-        })
-
-      }).catch(err => console.log("ERROR:", err))
-  })
+app.get("/proxy", (req, response) => {
+  const url = req.query.url
+  if (url && url.length > 0) {
+    fetch(url)
+    .then(res => res.body.pipe(response))
+    .catch(err => console.log(err))
+  }
 })
 
-
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Listening on port", PORT)
 })
